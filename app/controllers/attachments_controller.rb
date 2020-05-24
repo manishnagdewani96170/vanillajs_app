b3a0084a70672d3bf59d786fa5f59415746b92c0
Manish N <nagdewanimanis@gmail.com>
2020-05-24 23:40:41 +0530

    Setting up sample value as constant

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -2,7 +2,7 @@ class AttachmentsController < ApplicationController
   before_action :check_images_exist?, only: :create
   
   def index
-    @attachments = Attachment.all.sample(10)
+    @attachments = Attachment.all.sample(Attachment::RANDOM_COUNT)
     @images = @attachments.collect{ |c| c.image.url }
     render json: @images, status: :ok 
   end
@@ -13,10 +13,10 @@ class AttachmentsController < ApplicationController
       images << { image: img }  
     end
     @attachment = Attachment.create!(images)
-    if @attachment
-      redirect_to root_path, notice: "Uploaded successfully."
+    if @attachment.present?
+      redirect_to root_path, notice: 'Uploaded successfully.'
     else
-      redirect_to root_path, error: 'Something wnet wrong.'
+      redirect_to root_path, error: 'Something went wrong.'
     end    
   end
   
--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,4 +1,8 @@
 class Attachment < ApplicationRecord
+  #constants
+  RANDOM_COUNT = 10
+  #Uploader
   mount_uploader :image, FileUploader
+  #validations
   validates_presence_of :image
 end
